04336cd3dea8503e18e407b0192abc09da424b7f
verioska <rverioska01@gmail.com>
2019-04-11 16:31:03 -0400

    Updates

---
+++ precache-manifest.258ccd3c5076493d7d91628f263d3abd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e614b61dce542294b39f",
+    "url": "/Pinterest/static/css/main.97f9b2b2.chunk.css"
+  },
+  {
+    "revision": "e614b61dce542294b39f",
+    "url": "/Pinterest/static/js/main.d67f6654.chunk.js"
+  },
+  {
+    "revision": "dea190779213edba1912",
+    "url": "/Pinterest/static/js/runtime~main.61a2ba1d.js"
+  },
+  {
+    "revision": "234bc00da372a194b7ae",
+    "url": "/Pinterest/static/js/2.be9d155b.chunk.js"
+  },
+  {
+    "revision": "f03c48d976f35a12024dfa8973fe8664",
+    "url": "/Pinterest/index.html"
+  }
+];
